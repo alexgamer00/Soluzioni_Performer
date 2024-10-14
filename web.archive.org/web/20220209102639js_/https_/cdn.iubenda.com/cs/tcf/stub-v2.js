894c1baa0130a2f70a436d8726f35a63b47d6c9a
Alessio Magro <alex.gamer.unity@gmail.com>
2024-10-14 22:22:59 +0200

    primo

---
+++ web.archive.org/web/20220209102639js_/https_/cdn.iubenda.com/cs/tcf/stub-v2.js
@@ -0,0 +1,35 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  //let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+!function(n){var r={};function o(t){if(r[t])return r[t].exports;var e=r[t]={i:t,l:!1,exports:{}};return n[t].call(e.exports,e,e.exports,o),e.l=!0,e.exports}o.m=n,o.c=r,o.d=function(t,e,n){o.o(t,e)||Object.defineProperty(t,e,{enumerable:!0,get:n})},o.r=function(t){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(t,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(t,"__esModule",{value:!0})},o.t=function(e,t){if(1&t&&(e=o(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var n=Object.create(null);if(o.r(n),Object.defineProperty(n,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var r in e)o.d(n,r,function(t){return e[t]}.bind(null,r));return n},o.n=function(t){var e=t&&t.__esModule?function(){return t.default}:function(){return t};return o.d(e,"a",e),e},o.o=function(t,e){return Object.prototype.hasOwnProperty.call(t,e)},o.p="",o(o.s=384)}({384:function(t,e,n){n(385)()},385:function(t,e,n){var r=n(386);t.exports=function(){r();var t=void 0===window._iub?window._iub={}:window._iub,e=t.cmp||(t.cmp={});(e.VERSION||(e.VERSION={})).stubV2="0.2.0"}},386:function(t,e,n){"use strict";function a(t){return(a="function"==typeof Symbol&&"symbol"==typeof Symbol.iterator?function(t){return typeof t}:function(t){return t&&"function"==typeof Symbol&&t.constructor===Symbol&&t!==Symbol.prototype?"symbol":typeof t})(t)}t.exports=function(){for(var t,r,o="__tcfapiLocator",u=[],i=window,e=i;e;){try{if(e.frames[o]){t=e;break}}catch(t){}if(e===i.top)break;e=i.parent}t||(function t(){var e,n=i.document,r=!!i.frames[o];return r||(n.body?((e=n.createElement("iframe")).style.cssText="display:none",e.name=o,n.body.appendChild(e)):setTimeout(t,5)),!r}(),i.__tcfapi=function(){for(var t=arguments.length,e=new Array(t),n=0;n<t;n++)e[n]=arguments[n];if(!e.length)return u;"setGdprApplies"===e[0]?3<e.length&&2===parseInt(e[1],10)&&"boolean"==typeof e[3]&&(r=e[3],"function"==typeof e[2]&&e[2]("set",!0)):"ping"===e[0]?"function"==typeof e[2]&&e[2]({gdprApplies:r,cmpLoaded:!1,cmpStatus:"stub"}):u.push(e)},i.addEventListener("message",function(r){var o="string"==typeof r.data,t={};if(o)try{t=JSON.parse(r.data)}catch(r){}else t=r.data;var u="object"===a(t)?t.__tcfapiCall:null;u&&window.__tcfapi(u.command,u.version,function(t,e){var n={__tcfapiReturn:{returnValue:t,success:e,callId:u.callId}};r&&r.source&&r.source.postMessage&&r.source.postMessage(o?JSON.stringify(n):n,"*")},u.parameter)},!1))}}});
+
+}
+/*
+     FILE ARCHIVED ON 10:26:39 Feb 09, 2022 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 20:07:50 Oct 14, 2024.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.516
+  exclusion.robots: 0.023
+  exclusion.robots.policy: 0.014
+  esindex: 0.009
+  cdx.remote: 22.546
+  LoadShardBlock: 149.667 (6)
+  PetaboxLoader3.datanode: 101.31 (7)
+  PetaboxLoader3.resolve: 207.424 (2)
+  load_resource: 216.221
+*/
---
+++ web.archive.org/web/20220209103503js_/https_/cdn.iubenda.com/cs/iubenda_cs.js
@@ -0,0 +1,36 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  //let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+!function(e){"use strict";var o=0;function r(){var e="//web.archive.org/web/20220209103503/https://cdn.iubenda.com/cookie_solution/iubenda_cs/1.36.1/core-"+_iub.csConfiguration.lang+".js",t=document.querySelector('script[src="'+e+'"]');if(!t){t=document.createElement("script");var n=document.querySelector("script");t.src=e,t.addEventListener("error",(function(){++o<5&&(t.parentNode.removeChild(t),setTimeout((function(){r()}),10))})),n.parentNode.insertBefore(t,n)}}_iub.invTcfC=Date.now()-31104e6;_iub.csConfigLegacy=!1,_iub.GVL2=_iub.GVL2||129;_iub.cc='US-CA',r(),e.loadCore=r,Object.defineProperty(e,"__esModule",{value:!0})}({});
+
+
+}
+/*
+     FILE ARCHIVED ON 10:35:03 Feb 09, 2022 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 20:07:52 Oct 14, 2024.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.536
+  exclusion.robots: 0.029
+  exclusion.robots.policy: 0.02
+  esindex: 0.009
+  cdx.remote: 4.506
+  LoadShardBlock: 523.347 (6)
+  PetaboxLoader3.datanode: 134.088 (7)
+  PetaboxLoader3.resolve: 59.811 (2)
+  load_resource: 47.374
+*/
